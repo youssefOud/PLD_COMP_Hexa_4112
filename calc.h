b23fdca212dd6daf393fb46e6bfe43a9460156d1
Etienne Christophe <cetienne@if501-219-09.insa-lyon.fr>
2019-03-21 18:57:09 +0100

    visiteur changements terminés

--- calc.h
+++ calc.h
@@ -18,6 +18,7 @@
 #include "RightValueId.h"
 #include "RightValueNbr.h"
 
+// TODO Import new classes
 using namespace std;
 
 class  calc : public exprBaseVisitor {
@@ -31,8 +32,8 @@ public:
     return NULL;
   }
 
-  virtual antlrcpp::Any visitTypefct(exprParser::TypefctContext *ctx) override {
-    return visit(ctx->INT());
+  virtual antlrcpp::Any visitTypefct(exprParser::TypefctContext *context) override {
+    return visit(context->INT());
   }
 
   //il faut rendre le même type pas string une fois et int une fois
@@ -52,29 +53,32 @@ public:
   }
 
   virtual antlrcpp::Any visitDeclaration(exprParser::DeclarationContext *context) override {
-
-		Instruction * dec = new Declaration((std::string) context->ID()->getText(),(std::string) context->type()->getText());
-		return dec;
+  // TODO modifier
+    list<string> ids = (list<string>) visit(context->ids());
+    list<Instruction*> instructions;
+    for(list<string>::iterator it = ids.begin(); it != ids.end(); it++){
+		  Instruction * dec = new Declaration((std::string) (*it)->getText(),(std::string) context->type()->getText());
+      instructions.push_back(dec);
+    }
+		return instructions;
   }
 
   virtual antlrcpp::Any visitDefinition(exprParser::DefinitionContext *context) override {
-
-		Instruction * def = new Definition((RightValue *)visit(context->right()),(LeftValue *)visit(context->left()),(std::string) context->type()->getText());
+    //TODO ; modifier
+		Instruction * def = new Definition((Expression *)visit(context->expression()),(LeftValue *)visit(context->left()),(std::string) context->type()->getText());
 
 		return def;
   }
 
   virtual antlrcpp::Any visitAffectation(exprParser::AffectationContext *context) override {
-  		
-		Instruction * affS =new AffectationSimple((RightValue *)visit(context->right()),(LeftValue *)visit(context->left()),"");
+  	//TODO ; modifier
+		Instruction * affS =new AffectationSimple((Expression *)visit(context->expression()),(LeftValue *)visit(context->left()),"");
 		
 		return affS;
   }
 
   virtual antlrcpp::Any visitReturn(exprParser::ReturnContext *context) override {
-
-  		Instruction * ret = new Return((RightValue *)visit(context->right()));
-
+  		Instruction * ret = new Return((Expression *)visit(context->expression()));
 		return ret;
   }
 
@@ -87,22 +91,21 @@ public:
   virtual antlrcpp::Any visitCorpsRetour(exprParser::CorpsRetourContext *context) override {
 	 list<Instruction*> instructions;
 
-	instructions.push_back(visit(context->ret()));
+	  instructions.push_back(visit(context->ret()));
     return instructions ;
   }
 
   virtual antlrcpp::Any visitCorpsDeclareRet(exprParser::CorpsDeclareRetContext *context) override {
-  
-    list<Instruction*> instructions = { visit(context->declare()) ,visit(context->ret())};
+    list<Instruction*> declarations = visit(context->declare());
+    list<Instruction*> instructions;
+    for(list<Instruction*>::iterator it = declarations.begin(); it != declarations.end(); it++){
+      instructions.push_back((*it));
+    }
+    instructions.push_back(visit(context->ret()));
     return instructions ;
   }
 
   virtual antlrcpp::Any visitCorpsAffect(exprParser::CorpsAffectContext *context) override {
- 	//list<Instruction*> instructions;
-	//Affectation * i = dynamic_cast<Affectation*>((Declaration *)visit(context->declare()));
-	//instructions.push_back(i);
-    //instructions.splice(instructions.end(),visit(context->corps()));
-
     list<Instruction*> instructions = {visit(context->affect())};
     instructions.splice(instructions.end(),visit(context->corps()));
     return instructions;
@@ -116,16 +119,47 @@ public:
 
   virtual antlrcpp::Any visitCorpsDeclare(exprParser::CorpsDeclareContext *context) override {
     list<Instruction*> instructions;
-	instructions.push_back(visit(context->declare()));
+    list<Instruction*> declarations = visit(context->declare());
+    for(list<Instruction*>::iterator it = declarations.begin(); it != declarations.end(); it++){
+      instructions.push_back((*it));
+    }
     instructions.splice(instructions.end(),visit(context->corps()));
     return instructions;
   }
   
-  /*virtual antlrcpp::Any visitRet(exprParser::RetContext *context) override {
-   myfile << "visit ret" << std::endl; 
-   //return (int) stoi(ctx->NBR()->getText());
-   return NULL;
-  }*/
+  virtual antlrcpp::Any visitExprMult(exprParser::ExprMultContext *context) override {
+    Expression * expr = new ExprMult((Expression *) visit(context->expression(0)), (Expression *)visit(context->expression(1)));
+    return expr;
+  }
+  
+  virtual antlrcpp::Any visitExprAdd(exprParser::ExprAddContext *context) override {
+    Expression * expr = new ExprAdd((Expression *) visit(context->expression(0)), (Expression *)visit(context->expression(1)));
+    return expr;
+  }
+  
+  virtual antlrcpp::Any visitExprMinus(exprParser::ExprMinusContext *context) override {
+    Expression * expr = new ExprMoins((Expression *) visit(context->expression(0)), (Expression *)visit(context->expression(1)));
+    return expr;
+  }
+  
+  virtual antlrcpp::Any visitExprPar(exprParser::ExprParContext *context) override {
+    Expression * expr = new Expr((Expression *) visit(context->expression()));
+    return expr;
+  }
+  
+  virtual antlrcpp::Any visitRightValue(exprParser::RightValueContext *context) override {
+    return visitChildren((RightValue *)visit(context->right()));
+  }
+  
+  virtual antlrcpp::Any visitSimpleId(exprParser::SimpleIdContext *context) override {
+    return visit(context->ID());
+  }
+  
+  virtual antlrcpp::Any visitMultipleId(exprParser::MultipleIdContext *context) override {
+    list<string> ids = {visit(context->ID())};
+    ids.splice(ids.end(),visit(context->ids()));
+    return ids;
+  }
   
   list<Fonction*> getFonctions() {
     return fonctions;
@@ -133,8 +167,6 @@ public:
   
 protected :
   list<Fonction*> fonctions ;
-
-
 
 };
 
