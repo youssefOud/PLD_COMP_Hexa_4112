95d52fae5e2ac0644927b6fcd78574a589b98b92
Benchekroun Elghali <ebenchekro@if501-219-02.insa-lyon.fr>
2019-04-08 11:37:10 +0200

    Gestion des opérateurs de comparaison 2 !

--- antlr4/exprParser.h
+++ antlr4/exprParser.h
@@ -15,7 +15,8 @@ public:
     T__0 = 1, T__1 = 2, T__2 = 3, T__3 = 4, T__4 = 5, T__5 = 6, T__6 = 7, 
     T__7 = 8, T__8 = 9, T__9 = 10, T__10 = 11, T__11 = 12, T__12 = 13, T__13 = 14, 
     T__14 = 15, T__15 = 16, VOID = 17, INT = 18, CHAR = 19, NBR = 20, RETURN = 21, 
-    IF = 22, ELSE = 23, WHILE = 24, ID = 25, CARACTERE = 26, WS = 27
+    IF = 22, ELSE = 23, WHILE = 24, ID = 25, CARACTERE = 26, PREPRO = 27, 
+    COM1 = 28, COM2 = 29, WS = 30
   };
 
   enum {
--- test/test.c
+++ test/test.c
@@ -1,6 +1,8 @@
 int main() {
 	int a = 4;
 	int b = 12;
-	int c = (0!=a);
+	if(a==5){
+		int c = (0!=a);
+	}
 	return c;
 }
